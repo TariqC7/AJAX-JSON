ca097610f272fb65059444bed3d70f4389676d08
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2019-04-28 17:15:10 -0400

    Creating the category table

--- db.sql
+++ db.sql
@@ -12,3 +12,16 @@ LOCK TABLES 'author' WRITE;
 INSERT INTO 'author' VALUES (1,'Kit Harington'),(2,'Emilia Clarke'),(3,'Peter Dinklage'),(4,'Jason Momoa');
 
 UNLOCK TABLES;
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `name` varchar(255) DEFAULT NULL,
+  `book_id` int(10) unsigned DEFAULT NULL AUTO_INCREMENT,
+)ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+
+LOCK TABLES 'category' WRITE;
+
+INSERT INTO 'category' VALUES ('Fiction',1),('Autobiography',2),('Novel',3);
+
+UNLOCK TABLES;
+
