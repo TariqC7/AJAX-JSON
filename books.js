594600337612b7361c129315b7486ecb506c5130
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2019-04-28 17:28:29 -0400

    Error handling for ajax failure

--- books.js
+++ books.js
@@ -1,1 +1,7 @@
+(function() {
+    "use strict";
 
+    function ajaxFailure (ajax, exception) {
+        console.log("There was an error making the ajax request: " + "\nStatus: " + ajax.status + "  "
+          + ajax.statusText + "\nResponse: " + ajax.responseText);
+    }
