6a51af923dc3670be39b682ed72d51496922719d
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2019-04-28 17:22:18 -0400

    Finishing up books.php

--- books.php
+++ books.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-    <head>
+  <head>
         <title> Book Viewer </title>
 
           <script src="https://ajax.googleapis.com/ajax/libs/prototype/1.7.3.0/prototype.js"></script>
@@ -10,3 +10,9 @@
         <link rel="stylesheet" href="books.css">
   </head>
   <body>
+        <div id="category"></div>
+        <button id="list_books">List Books</button>
+        <br>
+        <div id="books"></div>
+  </body>
+</html>
