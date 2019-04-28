881c491f5d3724b43706d6dda567264693d92761
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2019-04-28 17:20:54 -0400

    The body of the book viewer html

--- books.php
+++ books.php
@@ -1,1 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Book Viewer </title>
 
+          <script src="https://ajax.googleapis.com/ajax/libs/prototype/1.7.3.0/prototype.js"></script>
+          <script type="text/javascript" src="books.js"></script>
+          <html>
+
+        <link rel="stylesheet" href="books.css">
+  </head>
+  <body>
