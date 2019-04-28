d17c0749b143b34836d38d4fca20568842d7de5e
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2019-04-28 17:37:31 -0400

    Making a fetch query

--- booklist.php
+++ booklist.php
@@ -9,3 +9,15 @@ function input($string=""){
     $string = trim($string);
     $string = stripslashes($string);
     return $string;
+}
+
+function fetchQuery($db, $sql){
+    try {
+        $smt = $db->prepare($sql);
+        $smt->execute();
+        $rows = $smt->fetchAll(PDO::FETCH_ASSOC);
+        return $rows;
+     } catch (PDOException $ex) {
+        error_log($ex->getMessage(), 3, "errors.txt");
+     }
+
