54097f638e08f47d04cf3b959eb6816f5a42d475
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2019-04-28 17:13:36 -0400

    Creating the author table

--- db.sql
+++ db.sql
@@ -1,1 +1,14 @@
+mysql --user=root --password
 
+DROP TABLE IF EXISTS `author`;
+CREATE TABLE `author` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `author_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+
+LOCK TABLES 'author' WRITE;
+
+INSERT INTO 'author' VALUES (1,'Kit Harington'),(2,'Emilia Clarke'),(3,'Peter Dinklage'),(4,'Jason Momoa');
+
+UNLOCK TABLES;
