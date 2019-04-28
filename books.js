98832f58f477f4165c34f688390638170be24440
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2019-04-28 17:30:43 -0400

    Making the display category function

--- books.js
+++ books.js
@@ -5,3 +5,14 @@
         console.log("There was an error making the ajax request: " + "\nStatus: " + ajax.status + "  "
           + ajax.statusText + "\nResponse: " + ajax.responseText);
     }
+
+    function displayCategory() {
+        var type = "list_category";
+        new Ajax.Request("booklist.php",
+            {
+            method: "post",
+            parameters: {type: type, json: false},
+            //contentType: "text/xml",
+            onSuccess: function (ajax) {
+                var category = ajax.responseXML.getElementsByTagName("category");
+                var i = 0;
