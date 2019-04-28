387fd7023637cbf3c636d5ab737fd3b483454fff
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2019-04-28 17:26:42 -0400

    Sanitizing the input to make sure that it is valid

--- booklist.php
+++ booklist.php
@@ -1,1 +1,11 @@
+<?php
+header('Content-type: text/xml');
+header('Pragma: public');
+header('Cache-control: private');
+header('Expires: -1');
 
+function input($string=""){
+    $string = htmlspecialchars($string);
+    $string = trim($string);
+    $string = stripslashes($string);
+    return $string;
